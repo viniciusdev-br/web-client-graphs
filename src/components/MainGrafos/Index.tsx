b7068075afff0f632a0e118e0d87a8734336ef08
Vinicius Chaves Botelho <viniciusvcb2002@gmail.com>
2022-06-10 14:56:27 -0300

    Tags de informação para criação do grafo finalizadas

--- src/components/MainGrafos/Index.tsx
+++ src/components/MainGrafos/Index.tsx
@@ -1,23 +1,40 @@
-import { Container, LeftFormContainer, OutputGraphs } from "./styles";
+import { Container, ContainerQuestion, LeftFormContainer, OutputGraphs } from "./styles";
 
 export function MainGrafos() {
     return (
         <Container>
             <LeftFormContainer>
                 <div>
-                    <p>Grafo orientado</p>
-                    <select >
+                    <ContainerQuestion>
+                        <label htmlFor="arestaOrientacao">Grafo orientado:</label>
+                        <select id="arestaOrientacao">
+                            <option value="Sim"> - </option>
+                            <option value="Sim">Sim</option>
+                            <option value="Não">Não</option>
+                        </select>
+                    </ContainerQuestion>                
+                    <label htmlFor="arestaPeso">As arestas tem pesos?</label>
+                    <select id="arestaPeso">
+                        <option value="Sim">-</option>
                         <option value="Sim">Sim</option>
                         <option value="Não">Não</option>
                     </select>
+
+                    <label htmlFor="NumVertices">Informe o número de vérties:</label>
+                    <input type="number" name="NumVertices" id="NumVertices" />
+
+                    <button type="button">Ok!</button>
                 </div>
 
                 <div>
-                    <p>Grafo orientado</p>
-                    <select >
-                        <option value="Sim">Sim</option>
-                        <option value="Não">Não</option>
-                    </select>
+                    <label htmlFor="origemVertice">Vértice de origem:</label>
+                    <input type="text" name="origemVertice" id="origemVertice" />
+
+                    <label htmlFor="destinoVertice">Vértice de destino:</label>
+                    <input type="text" name="destinoVertice" id="destinoVertice" />
+
+                    <label htmlFor="destinoVertice">Informe o peso desta conexão:</label>
+                    <input type="text" name="destinoVertice" id="destinoVertice" placeholder="Digite o peso aqui..."/>
                 </div>
 
             </LeftFormContainer>
--- src/components/MainGrafos/styles.ts
+++ src/components/MainGrafos/styles.ts
@@ -1,13 +1,19 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const Container = styled.main`
     display: 'flex';
+    flex-direction: row;
     background-color: #D9D9D9;
     width: 81rem;
     height: 45rem;
     margin: 2rem auto;
     border-radius: 15px;
 `
+
+export const ContainerQuestion = styled.div`
+    width: 351px;
+`
+
 export const LeftFormContainer = styled.div`
     width: 40%;
     height: 100%;
@@ -17,6 +23,8 @@ export const LeftFormContainer = styled.div`
 
     div {
         background-color: #fff;
+        display: flex;
+        flex-direction: column;
         height: 35%;
         width: 100%;
         border-radius: 10px;
